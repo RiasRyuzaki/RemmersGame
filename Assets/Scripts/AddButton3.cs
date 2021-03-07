0a5cfd3fd2be0818f765a1c125e53f72944756e5
Rias Ryuzaki <lukasz.szturemski@gmail.com>
2021-03-07 15:47:01 +0100

    Adding Project

---
+++ Assets/Scripts/AddButton3.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddButton3 : MonoBehaviour
+{
+    [SerializeField]
+    private Transform puzzleField;
+
+    [SerializeField]
+    private GameObject bnt;
+
+
+    void Awake()
+    {
+        for (int i = 0; i < 24; i++)
+        {
+            GameObject button = Instantiate(bnt);
+            button.name = "" + i;
+            button.transform.SetParent(puzzleField, false);
+        }
+    }
+
+}
